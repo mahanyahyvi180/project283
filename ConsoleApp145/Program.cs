88ad43897c619fc033dd3e9622aaef04a07a0f05
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-12-23 21:54:27 +0330

    Add files via upload

---
+++ ConsoleApp145/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp145
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] characterArray = { 'b', 'i', 'r', 't', 'h', ' ', 'd', 'a', 'y' };
+
+            var originalString = "welcome to c# programing";
+
+            var string1 = originalString;
+            var string2 = new string(characterArray);
+            var string3 = new string(characterArray, 6, 3);
+            var string4 = ("C", 5);
+
+            Console.WriteLine($"string1=\"{string1}\"\n" + $"string2=\"{string2}\"\n" + $"string3=\"{string3}\"\n" + $"string4=\"{string4}\"\n");
+            Console.ReadLine();
+            
+        }
+    }
+}
